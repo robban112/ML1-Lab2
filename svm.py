445f3e88064874cee39777e5fe7a66ec803b84b4
Oskar Ek <oskarek24@gmail.com>
2018-02-14 15:07:05 +0100

    added indicator function plus neccessary functions around it

--- svm.py
+++ svm.py
@@ -1,6 +1,7 @@
 import numpy, random ,math
 from scipy.optimize import minimize
 import matplotlib.pyplot as plt
+from collections import namedtuple
 
 global t
 global P
@@ -21,6 +22,37 @@ def linear_kernel(a, b):
     assert len(a) == len(b)
     return sum([a[i][0]*b[i] for i in range(len(b))])
 
+# A data point along with its corresponding target and alpha value
+DataPointInfo = namedtuple('DataPointInfo', ['point','target','alpha'])
+
+def non_zero(n):
+    "True if the number is separate from 0 with a small epsilon"
+    return abs(n) > 0.00001
+
+def is_support_vector(p):
+    "True iff the point in the DataPointInfo object is a support vector"
+    return non_zero(p.alpha)
+
+def support_vectors(ps):
+    "Filter only the support vectors from the data points"
+    return filter(is_support_vector, ps)
+
+def ind_no_b(s, ps, K):
+    """The summation that appears in the indicator function,
+    as well as in the calculation of b.
+    """
+    sup_vecs = support_vectors(ps)
+    sum_f = lambda p: p.alpha*p.target+K(s,p.point)
+    return sum(map(sum_f, sup_vecs))
+
+def ind(s, ps, K, b):
+    "The indicator function for vector s"
+    return ind_no_b(s, ps, K) - b
+
+def calc_b(ps, K):
+    "Calculate the b value"
+    s, s_t, _ = next(support_vectors(ps))
+    return ind_no_b(s, ps, K) - s_t
 
 def main():
     N=1
